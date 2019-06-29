de53d2e4ebc677319b0c855fea13c2d94b5b5398
Jan Kotas <kotasjn@gmail.com>
2019-06-29 17:27:21 +0200

    Homepage text changed.

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="card">
-        <div class="card-header">Example Component</div>
+        <div class="card-header">Vítejte!</div>
         <div class="card-body">
-            {{ welcome }}
+            Tato aplikace je součástí bakalářské práce Jana Kotase pod vedením Ing. Lukáše Semeráda na Fakultě informačních technologií Vysokého učení technického v Brně. Cílem aplikace je demonstrovat možnosti využití masově používaných moderních technologií ke zkvalitnění správy veřejných prostor v obcích. Aplikace je založena na principu přímé interakce občanů se správci měst. Díky aplikaci mají občané možnost během několika minut nahlásit problémy z jejich okolí, kterými by se mělo město zabývat. Na druhou stranu města mají díky hlášením od občanů dobrý přehled o tom, jaké problémy občany jejich měst trápí a díky dlouhodobé evidenci v systému mohou analyzovat, které části měst jsou nejvíce problematické.
         </div>
     </div>
 </template>
@@ -10,11 +10,6 @@
 <script>
     export default {
         name: "home",
-        computed: {
-            welcome() {
-                return this.$store.getters.welcome
-            }
-        }
     }
 </script>
 
