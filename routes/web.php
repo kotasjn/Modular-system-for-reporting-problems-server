609907fac1da678140fd0dcb97e4f282d5ba94e2
Jan Kotas <kotasjn@gmail.com>
2019-07-05 12:45:29 +0200

    Renamed main view

--- routes/web.php
+++ routes/web.php
@@ -11,34 +11,12 @@
 |
 */
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-
-/**
- * Auth routes with Socialite support
- */
-
-/*
-Auth::routes();
-
-Route::get('/login/{social}','Auth\LoginController@socialLogin')->where('social','facebook|google');
-
-Route::get('/login/{social}/callback','Auth\LoginController@handleProviderCallback')->where('social','facebook|google');
-
-*/
 
 //Route::get('b53c0d111e978eb6ffd484179e31f330', 'ArtisanController@passportInstall');
 
 Route::view('/privacy_policy', 'privacy');
 
 Route::get('{any}', function () {
-    return view('welcome');
+    return view('main');
 })->where('any', '.*');
-
-
-/*
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('/territory/{territory}', 'TerritoryController@show');
-});
-*/
